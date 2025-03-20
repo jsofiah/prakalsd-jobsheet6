fe2799d11be9092680e26027ad72584bb89441d8
jsofiah <j.sofiah18@gmail.com>
2025-03-20 11:29:07 +0700

    Percobaan 4

--- MahasiswaDemo21.java
+++ MahasiswaDemo21.java
@@ -35,5 +35,9 @@ public class MahasiswaDemo21 {
         System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC): ");
         list.selectionSort();
         list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC): ");
+        list.insertionSort();
+        list.tampil();
     }
 }
