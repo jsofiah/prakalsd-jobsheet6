e4b13f06459d905d63bfeec86a780e94165fb92c
jsofiah <j.sofiah18@gmail.com>
2025-03-20 10:20:07 +0700

    Percobaan 2 modifikasi

--- MahasiswaBerprestasi21.java
+++ MahasiswaBerprestasi21.java
@@ -1,6 +1,11 @@
 public class MahasiswaBerprestasi21 {
-    Mahasiswa21[] listMhs = new Mahasiswa21[5];
+    
+    Mahasiswa21[] listMhs;
     int idx;
+    public MahasiswaBerprestasi21(int kapasitas) {
+        listMhs = new Mahasiswa21[kapasitas];
+        idx = 0;
+    }
     void tambah (Mahasiswa21 m){
         if(idx < listMhs.length){
             listMhs[idx] = m;
--- MahasiswaDemo21.java
+++ MahasiswaDemo21.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
 public class MahasiswaDemo21 {
     public static void main(String[] args) {
-        MahasiswaBerprestasi21 list = new MahasiswaBerprestasi21();
-        Mahasiswa21 m1 = new Mahasiswa21("123", "Zidan", "2A", 3.2);
-        Mahasiswa21 m2 = new Mahasiswa21("124", "Ayu", "2A", 3.5);
-        Mahasiswa21 m3 = new Mahasiswa21("125", "Sofi", "2A", 3.1);
-        Mahasiswa21 m4 = new Mahasiswa21("126", "Sita", "2A", 3.9);
-        Mahasiswa21 m5 = new Mahasiswa21("127", "Miki", "2A", 3.7);
+        Scanner input21 = new Scanner(System.in);
+        
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = input21.nextInt();
+        input21.nextLine();
+        
+        MahasiswaBerprestasi21 list = new MahasiswaBerprestasi21(jumlah);
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Masukkan data mahasiswa ke " + (i+1) + ": ");
+            System.out.print("Nama: ");
+            String nama = input21.nextLine();
+            System.out.print("NIM: ");
+            String nim = input21.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = input21.nextLine();
+            System.out.print("IPK: ");
+            double ipk = input21.nextDouble();
+            input21.nextLine();
+            System.out.println("===================================");
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+            Mahasiswa21 m = new Mahasiswa21(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
 
         System.out.println("Data mahasiswa sebelum sorting: ");
         list.tampil();
